9bb8a18f01c0482557dcb19f36ba583809a05090
Ydjeen <Ydjeeen@gmail.com>
2023-12-03 12:14:37 +0100

    Day 1 Task 2 completed

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -17,6 +17,16 @@ pub(crate) fn task1(){
 }
 
 pub(crate) fn task2(){
+    let file_content = utils::read_file_lines(INPUT.to_owned());
+    let lines = file_content.split("\n");
+    let mut sum = 0;
+    for line in lines {
+        let number = get_dig_and_str_number_from_line(line);
+        sum = sum + number;
+    }
+    println!("Sum is equal to:{}", sum);
+
+    println!("Task2 executed")
 }
 
 
@@ -33,5 +43,39 @@ fn get_number_from_line(line: &str) -> u32{
             last_char = c.to_digit(10).unwrap();
         }
     }
+    return first_char + last_char;
+}
+
+const DIGIT_WORDS: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+fn get_dig_and_str_number_from_line(line: &str) -> u32{
+    let mut first_initialized = false;
+    let mut found_digit_check: bool = false;
+    let mut first_char = 0;
+    let mut last_char = 0;
+    let mut found_digit = 0;
+    for (i, c) in line.chars().enumerate() { 
+        found_digit_check = false;
+        if c.is_numeric(){
+            found_digit = c.to_digit(10).unwrap();
+            found_digit_check = true;
+        } else{
+            let line_slice = &line[i..];
+            for (i, word) in DIGIT_WORDS.iter().enumerate(){
+                if line_slice.starts_with(word){
+                    found_digit = (i as u32)+1;
+                    found_digit_check = true;
+                }
+            }
+        }
+        if found_digit_check{
+            if !first_initialized{
+                first_char = 10 * found_digit;
+                first_initialized = true;
+            }
+            last_char = found_digit;
+        }
+    }
+    println!("{line}, {first_char}, {last_char}");
     return first_char + last_char;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ pub mod days;
 pub mod utils;
 fn main() {
     days::day1::task1();
+    days::day1::task2();
 }
